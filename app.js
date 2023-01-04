f137271f216ec7d9ac6bb7fc282be73b30f2a4d1
marianorigo18 <marianowagnerrigo@gmail.com>
2023-01-04 19:49:17 -0300

    first commit

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const form = document.querySelector('.form');
 const inputNumber = document.querySelector('.input-number');
-
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
+    if(inputNumber.value.trim() == ''){
+       return console.log('El campo esta vacio')
+    }
+    if(!Number(inputNumber.value.trim())){
+        return console.log('El valor no es numerico')
+    }
+    if(inputNumber.value.trim() > 10){
+        return console.log('El campo no debe ser mayor a 10')
+    }
+    subirMastil(Number(inputNumber.value.trim()));
 
-    if(inputNumber.value.trim() === ''){
-        console.log('el campo debe contener almenos 1 caracter')
-    }
-    if(Number(inputNumber.value.trim()) > 10){
-        console.log('el campo debe ser menor a 10')
-    }
-    if(Number){
-        subirMastil(inputNumber)
-    }
 })
-/*
 function subirMastil(valor){
     for(let i = 1; i <= valor; i++){
-        if(i < inicio){
-            console.log(`subo ${i} metro`)
-        }else{
-            console.log(`subo hasta ${i} metros`)
-        }
-    }
+            if(i < valor){
+                    console.log(`subo ${i} metro`)
+            }else{
+                    console.log(`subio hasta ${i} metros`)
+                }
+            }
 }
-*/
